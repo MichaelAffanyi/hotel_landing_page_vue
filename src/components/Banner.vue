11309ad0e345e593d50b9620fb05501393323087
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-03-24 11:25:47 +0000

    Create Banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        name: 'Banner',
+
+    }
+</script>
+
+<template>
+    <div class="banner"></div>
+    <div class="menu">
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+            <span></span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .banner {
+       width: 100%;
+       height: 565px;
+       background: url("../assets/bg-main.png") no-repeat center;
+    }
+    .menu {
+        width: 840px;
+        height: 74px;
+        background: red;
+        margin: 71px auto 139px;
+    }
+</style>
