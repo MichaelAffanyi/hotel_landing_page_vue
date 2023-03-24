17346cf5dda6fbf6f4811284b9ea7db776ceac88
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-03-24 11:24:00 +0000

    Create app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Nav from './components/Nav.vue';
+import Header from './components/Header.vue';
+import Banner from './components/Banner.vue';
+
+export default {
+  components: {
+    Nav,
+    Header,
+    Banner
+  }
+}
+</script>
+
+<template>
+  <Nav />
+  <Header />
+  <Banner />
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
