8de2cd09914c5b76e6fee58f30ce64f6f75001dd
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-08-23 17:07:20 +0000

    added video

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Nav from './components/Nav.vue';
 import Header from './components/Header.vue';
 import Banner from './components/Banner.vue';
-// import videoEn from '@/assets/videos/EDU_Video_Final_V2_eng.mp4';
+import videoEn from '@/assets/videos/EDU_Video_Final_V2_eng.mp4';
 import videoDu from '@/assets/videos/Edu_Video_Deutsch_V1.mp4'
 
 export default {
@@ -16,13 +16,14 @@ export default {
   },
   data() {
     return {
-      // videoEn,
+      videoEn,
       videoDu
     }
   },
   methods: {
     getNavigator() {
-      return navigator.language === 'de'
+      console.log(navigator.language)
+      return navigator.language
     }
   }
 }
@@ -39,7 +40,7 @@ export default {
 <!--  </div>-->
   <div>
   <video controls width="320">
-    <source :src="getNavigator() && videoDu" type="video/mp4">
+    <source :src="getNavigator() === 'de'? videoDu : videoEn" type="video/mp4">
   </video>
   </div>
 </template>
